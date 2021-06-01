0507cc78c295721076a5cfbc761607428c9a3f8e
Grant Gannon <gtganno@gmail.com>
2021-06-01 19:58:53 -0400

    Initial Commit

---
+++ src/main/java/us/carrotcraft/borderadapt/Borderadapt.java
@@ -0,0 +1,17 @@
+package us.carrotcraft.borderadapt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Borderadapt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
